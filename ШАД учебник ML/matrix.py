3a0f2be65814bfabfbe4f0d474fa1502a8eb27de
Artem <73838119+artgor261@users.noreply.github.com>
2024-07-31 14:20:57 +1000

    Delete ШАД учебник ML/matrix.py

--- ШАД учебник ML/matrix.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def construct_matrix(first_array, second_array):
-    return np.hstack([first_array.reshape((len(first_array), 1)),
-                      second_array.reshape((len(second_array), 1))])
